223512230ee43fc68c534fb77e80578a9199c20b
Rodrigo Jimenez <correo.rodrigo@gmail.com>
2012-02-28 19:36:02 -0600

    authentication

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
+var express   	= require('express')
+  , routes 	= require('./routes')
+  , everyauth 	= require('everyauth')
+  , Promise 	= everyauth.Promise
+  , util 	= require('util')
+  , config 	= require('konphyg')(__dirname + '/config');
 
-var app = module.exports = express.createServer();
+var twitterConfig = config('twitterapp');
+
+everyauth.twitter.consumerKey(twitterConfig.key).consumerSecret(twitterConfig.secret).findOrCreateUser(function(session, accessToken, accessTockenSecret,twiiterUserData){	
+	var promise = new Promise();
+	//users.findOrCreateByTwitterData(twitterUserData, accessTocken, accessTockenSecret, promise);
+	return promise;
+}).redirectPath('/chat');
+
+var app = express.createServer();
 
 // Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: "90ndsj9dfdsfro"}));
+  app.use(everyauth.middleware());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  everyauth.helpExpress(app);
 });
 
 app.configure('development', function(){
