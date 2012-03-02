d80e55e5ab64ebc5afb33f5de2998f7985501995
Rodrigo Jimenez <correo.rodrigo@gmail.com>
2012-03-01 20:47:29 -0600

    fix: mongo not retrieving user

--- public/js/twitterchat.js
+++ public/js/twitterchat.js
@@ -1,22 +1,24 @@
 $(function(){
-    var socket = new io.Socket('localhost', {port: 3000});
-    socket.connect();
+    var socket = io.connect('http://localhost');
 
     var txtMsg = $("#txtMessage");
     var btnSend = $("#btnSend");
     var messages = $('#message ul');
     txtMsg.focus();
     
-    socket.on('message', function(){
-	var data = message.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
-	messages.prepend('<li>' + data + '</li>');
+    socket.on('message', function(data){
+	var strData = data.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+	messages.prepend('<li>' + strData + '</li>');
     });
 
+    socket.on('joined', function(data) {        
+        messages.prepend('<li>' + data + 'joined.' + '</li>');
+    });
 
     function sendMessage(){
 	var msg = txtMsg.attr('value');
         if (msg) {
-            socket.emit(msg);
+            socket.emit('message', msg);
         }
         txtMsg.focus();
         txtMsg.attr('value', '');
