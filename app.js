04ff2a2a7b8981fd4bdb69d8432c4627c8cfcecb
Rodrigo Jimenez <correo.rodrigo@gmail.com>
2012-03-16 18:56:14 -0600

    jade layout templates fixed

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var MemStore = express.session.MemoryStore;
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', {layout: false});
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({secret: config.appConfig.application.secret}));
