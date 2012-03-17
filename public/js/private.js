035f6c3ac07a1e71965f638698365e4418d18223
Rodrigo Jimenez <correo.rodrigo@gmail.com>
2012-03-16 19:11:11 -0600

    adding private channel

--- public/js/private.js
+++ public/js/private.js
@@ -1,10 +1,10 @@
-$(function(){
+$(function() {
     var socket = io.connect(document.location.href);
     
     var txtMsg = $("#txtMessage");
     var btnSend = $("#btnSend");
     var messages = $("#msgs");
-    var users = $("#usrs");
+
     var screenName = $("#screenName").val();
     var twitterId  = $("#twitterId").val();
     txtMsg.focus();
@@ -18,49 +18,11 @@ $(function(){
 
     socket.on('joined', function(data) {
         messages.prepend('<li class="joined">@' + data.screenName + ' joined.' + '</li>');
-	addUser(data);
     });
 
     socket.on('leaved', function(data) {
-	messages.prepend('<li class="joined">@' + data.screenName + ' disconnected.' + '</li>');
-	removeUser(data);
+	messages.prepend('<li class="joined">@' + data.screenName + ' disconnected.' + '</li>');	
     });
-
-    socket.on('getOnlineUsers', function(onlineUsers) {
-	users.find("li").remove();
-	if (onlineUsers) {
-		for(var i=0; i < onlineUsers.length; i++) {
-			addUser(onlineUsers[i]);
-		}
-	}
-    });
-
-    socket.on('openPrivate', function (data) {	 
-	if (twitterId == data.sender.twitterId) {
-		var pUser = "private chat with @"+data.target.screenName;
-		window.open("private", pUser, null);	 
-	}
-
-	if (twitterId == data.target.twitterId) {
-		var pUser = "private chat with @"+data.sender.screenName;
-		 window.open("private", pUser, null);
-	}
-    });  
-
-    function addUser(data) {
-	if (!data) return;
-	if (users.find("input:hidden[value='"+data.twitterId+"']").length == 0) {
-		var hidId = '<input type="hidden" class="twitterId" value="' + data.twitterId + '" />';
-		var hidName = '<input type="hidden" class="screenName" value="' + data.screenName + '" />';
-        	var strLi = '<li>@' + data.screenName + hidId + hidName + '</li>';
-        	users.prepend(strLi);
-	}
-    }
-    
-    function removeUser(data) {
- 	if (!data) return;
-        users.find("input:hidden[value='"+data.twitterId+"']").parent().remove();
-    }
 
     function sendMessage() {	
 	var msg = txtMsg.attr('value');
@@ -77,15 +39,4 @@ $(function(){
     });
     
     btnSend.click(sendMessage);
-
-    users.delegate('li', 'dblclick', function() {
-	var hidId = $(this).find("input.twitterId").val();
-	var hidName = $(this).find("input.screenName").val();	
-	if (hidId != twitterId && hidName != screenName) {
-		var sender = {screenName: screenName, twitterId: twitterId};
-		var target = {screenName: hidName, twitterId: hidId};
-		var data = {sender: sender, target: target};
-		socket.emit("openPrivate", data);
-	}
-    });
 });
